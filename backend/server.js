9c8a9dfc040ce35d613c148d0f05d2955fa77adf
Suhail Mohamed <88363214+suhail-mhd@users.noreply.github.com>
2022-08-25 20:54:07 +0100

    deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000
 const dbConnection = require('./db')
 require('dotenv').config()
 const cors = require('cors');
+const path = require('path')
 const adminRoute = require('./Routes/adminRoutes')
 const userRoute = require('./Routes/userRoutes')
 
@@ -32,12 +33,15 @@ app.use(cors())
 app.use('/api/user',userRoute)
 app.use('/api/admin',adminRoute)
 
+// --------- deployment ----------
+
+__dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/build')));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 } else {
     app.get("/", (req, res) => {
